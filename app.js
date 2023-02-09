b895c941c8105e5a5e16120b95145ed3bd6f4204
megabebrik <113171956+megabebrik@users.noreply.github.com>
2023-02-09 08:24:23 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+/*let a = 0;
+setInterval(function() {
+    console.log(a++);
+}, 1000);*/
+
+// const fs = require('fs')
+// let text = fs.readFileSync('Новый текстовый документ (2).txt', 'utf8');
+// setInterval(function() {
+//     fs.writeFileSync('ssss.txt', text);-
+//     console.log(text);
+// }, 1000);
+
+// for (let i = 0; i < 150; i++){
+
+// console.log("Hello World!")
+// }
+
+for (let i =2; i <20; i++){
+    if(i % 2 === 0){
+        console.log(i);
+    }
+}
